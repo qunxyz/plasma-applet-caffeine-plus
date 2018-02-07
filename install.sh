e45df43c78481e348fb4083d20cd89f22e6808aa
Gordon Yau <qunxyz@gmail.com>
2018-02-07 09:54:15 +0800

    test install shell

--- install.sh
+++ install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+SYS_TYPE=""
+PKG_CMD=""
+
+if [ -f /usr/bin/apt ]; then
+	SYS_TYPE="Debian"
+	PKG_CMD="apt-get"
+	#${PKG_CMD} install cmake extra-modules build-essential gettext qtdeclarative5-dev libkf5kio-dev plasma-framework-dev
+	${PKG_CMD} install extra-cmake-modules build-essential gettext qtdeclarative5-dev libkf5kio-dev plasma-framework-dev
+elif [ -f /usr/bin/dnf ]; then
+	SYS_TYPE="Fedora"
+	PKG_CMD="dnf"
+elif [ -f /usr/bin/yum ]; then
+	SYS_TYPE="RedHat"
+	PKG_CMD="yum"
+elif [ -f /usr/bin/pacman ]; then
+	SYS_TYPE="ArchLinux"
+	PKG_CMD="pacman"
+fi
+
+#echo "SYS_TYPE: ${SYS_TYPE} PKG_CMD: ${PKG_CMD}"
+
+#exit
+##########################################
+
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
 cd $DIR
 
