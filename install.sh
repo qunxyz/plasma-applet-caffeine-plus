f1944a32e2ca1be5315f78c32c83b07ceac462a2
Gordon Yau <qunxyz@gmail.com>
2018-02-07 15:39:41 +0800

    test install shell

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ elif [[ -f /usr/bin/dnf || -L /usr/bin/dnf ]]; then
 elif [ -f /usr/bin/zypper ]; then
 	SYS_TYPE="openSUSE"
 	PKG_CMD="zypper"
-	sudo ${PKG_CMD} install gcc cmake extra-cmake-modules libqt5-qtdeclarative-devel plasma-framework-devel kio-devel kwindowsystem-devel
+	#sudo ${PKG_CMD} install gcc cmake extra-cmake-modules libqt5-qtdeclarative-devel plasma-framework-devel kio-devel kwindowsystem-devel
+	sudo ${PKG_CMD} install gcc cmake extra-cmake-modules libqt5-qtdeclarative-devel plasma-framework-devel kio-devel
 elif [ -f /usr/bin/pacman ]; then
 	SYS_TYPE="ArchLinux"
 	PKG_CMD="pacman"
